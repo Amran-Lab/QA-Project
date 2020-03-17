548b514355aa71e9b11368eb43eb049c62e4019d
Amran-Lab <amran3210@gmail.com>
2020-03-17 12:04:46 +0000

    installation

--- script/before_installation.sh
+++ script/before_installation.sh
@@ -5,5 +5,5 @@ sudo apt install python3-pip -y
 sudo apt install python3-venv -y
 sudo apt-get install libmysqlclient-dev -y
 
-rm -r /var/lib/jenkins/workspace/pipeline1/venv
+
 python3 -m venv venv
