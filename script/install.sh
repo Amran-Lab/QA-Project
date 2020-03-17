12dfe7438bb3441b0fa98ce519eff43d60ce7372
Amran-Lab <amran3210@gmail.com>
2020-03-17 11:05:54 +0000

    installation

---
+++ script/install.sh
@@ -0,0 +1,7 @@
+source venv/bin/activate
+
+pip3 install flask
+pip3 install flask_mysqldb
+pip3 install -U pytest
+pip3 install urllib3
+pip3 install coverage
--- script/installation.sh
+++ script/installation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source venv/bin/activate
 
-pip3 install flask
-pip3 install flask_mysqldb
-pip3 install -U pytest
-pip3 install urllib3
- pip3 install coverage
+#pip3 install flask
+#pip3 install flask_mysqldb
+#pip3 install -U pytest
+#pip3 install urllib3
+#pip3 install coverage
 source  ~/.bashrc
 
 python3 /var/lib/jenkins/workspace/pipeline1/app.py
