d868c4e35bacf0aceaa78c1d7824bde064d99fc8
Amran-Lab <amran3210@gmail.com>
2020-03-17 12:03:04 +0000

    installation

--- script/make_service.sh
+++ script/make_service.sh
@@ -2,7 +2,7 @@
 sudo cp /var/lib/jenkins/workspace/pipeline1/script/flask.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable flask.service
-
+sudo systemctl stop flask.service
 
 sudo systemctl start flask.service
 /var/lib/jenkins/workspace/pipeline1/venv/bin/pytest /var/lib/jenkins/workspace/pipeline1/testing.py
